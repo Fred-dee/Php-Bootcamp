052267e70a2d6bba5b170d7cc16613b3e527edb0
Fred-Dee <fred.dilapisho@gmail.com>
2018-10-03 23:11:51 +0200

    Enjoying the challenge, ex02 seems done

--- day02/ex02/magnifying_glass.php
+++ day02/ex02/magnifying_glass.php
@@ -1,6 +1,27 @@
 #!/usr/bin/php
 <?php
 
+$file;
 
+if ($argc > 1)
+{
+	$file = file_get_contents($argv[1]);
+	$nb = preg_match("/title=\"([a-z |\\s|A-Z]+)\"/",  $file);
+	$file = preg_replace_callback("/(title=\")([a-z |\\s|A-Z]+)(\")/",
+		function ($matches)
+		{
+			return($matches[1].strtoupper($matches[2]).$matches[3]);
+		}, $file);
+	
+	//echo "The number of matches is: ".$nb."\n";
+	$nb = preg_match("/(<\s*a[^>]*>)(.*?)(<)/",  $file);
+	$file = preg_replace_callback("/(<\s*a[^>]*>)(.*?)(<)/",
+		function($matches)
+		{
+			return($matches[1].strtoupper($matches[2]).$matches[3]);
+		}, $file);
+	echo "The number of matches is: ".$nb."\n";
+	echo $file."\n";
+}
 
 ?>
